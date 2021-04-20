1998d2e8b2ebaf19781da6dee8984081c4ba0698
Ianeskiii <50773395+Ianeskiii@users.noreply.github.com>
2021-04-20 01:04:09 -0400

    Update index.blade.php

--- resources/views/ServerDetails/index.blade.php
+++ resources/views/ServerDetails/index.blade.php
@@ -1,3 +1,29 @@
-<div>
-{{ $date }} {{ $timezone }} {{ $time }}
+<!DOCTYPE html>
+<html>
+<title>Server Details</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button"><b>Server</b> Details</a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="#projects" class="w3-bar-item w3-button">{{ $timezone }}</a>
+      <a href="#about" class="w3-bar-item w3-button">{{ $time }}</a>
+      <a href="#contact" class="w3-bar-item w3-button">{{ $date }}</a>
+    </div>
+  </div>
 </div>
+
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+</footer>
+
+</body>
+</html>
